c5b0926005ea98a8f5fb17000e0b02053d2e83ed
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-06-10 09:03:39 -0300

    new messsage error

--- src/modules/contracts/acceptContracts/AcceptContractsUseCase.ts
+++ src/modules/contracts/acceptContracts/AcceptContractsUseCase.ts
@@ -47,8 +47,8 @@ export class AccecptContractsUseCase {
       throw new AppError("route not found");
     }
 
-    if (ship.fuel_level <= routes.fuel_consumption || routes.route === false) {
-      throw new AppError("The travel is not possible");
+    if (ship.fuel_level <= routes.fuel_consumption) {
+      throw new AppError("The fuel is not enough to travel");
     }
 
     const acceptedContract = await prisma.contracts.findUnique({
